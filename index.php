230117f50b64eda6ecbab4512a4f7ccc494b77fb
anamika <ganamika063@gmail.com>
2020-11-13 13:08:03 +0530

    task completed

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
       <title>Contact-Us</title>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
       <link href="index.css" rel="stylesheet"/>
+      <link rel="stylesheet" type="text/css" href="jquery/jquery-ui.css">
+
  </head>
   <body>
       <div class="container-fluid">
@@ -24,7 +26,8 @@
               <input type="Phone" name="contact" id="contact" class="form-control form-rounded" placeholder="contact number" required=""><br>
 
               <label for="dob"> DOB </label>
-              <input type="date" name="dob" id="dob" class="form-control form-rounded"  placeholder="dd-mm-yyyy" required="" onblur ="ageCount()">  <br>
+              <input type="text" name="dob" id="dob" class="form-control form-rounded"  placeholder="dd-mm-yyyy" required="" onblur ="ageCount()">  <br>
+            <!--  // Age:<input type="text" id="age"> -->
               
              <div class="g-recaptcha" data-sitekey="<?php echo "6Leba-IZAAAAAM_AQUmocYuYPD2ZOjMT7NIMkJB8" ?>"></div>
               <?php //reCAPTCHA validation
@@ -53,35 +56,24 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-  <script src="dist/jquery.validate.min.js"></script>
+   
+    <script src="jquery/jquery-ui.js"></script>
+     <script src="jquery/jquery.js"></script>
+
   <!-- Recaptcha
    -->
    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
   <script type="text/javascript">
-     function ageCount() {
-      var date1 = new Date();
-      var dob = document.getElementById("dob").value;
-      var date2 = new Date(dob);
-      var pattern = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
-      //Regex to validate date format (dd/mm/yyyy)       
-      if (pattern.test(dob)) {
-          var y1 = date1.getFullYear();
-          //getting current year            
-          var y2 = date2.getFullYear();
-          //getting dob year            
-          var age = y1 - y2;
-          //calculating age                       
-          document.getElementById("ageId").value = age;
-          doucment.getElementById("ageId").focus ();
-          return true;
-      } else {
-          alert("Invalid date format. Please Input in (dd/mm/yyyy) format!");
-          return false;
-      }
-
-  }
+     $(document).ready(function()){
+      var age="";
+      $('#dob').datepicker({
+        onSelect :function(value,ui){
+          var today=new Date();
+          age =today.getFullYear()- ui.selectedYear;
+          $('#age').val(age);
+        },
+      })
+     }
   </script>
 
   </body>
